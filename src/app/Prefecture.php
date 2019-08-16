642cbe83b9b3527fd901844f5bb5408d8907ba45
Hiroshi <hermit255@gmail.com>
2019-08-16 14:48:49 +0900

    prefecturesモデルを調整

--- src/app/Prefecture.php
+++ src/app/Prefecture.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prefecture extends Model
 {
-    //
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'prefectures';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'surfix' => '',
+    ];
 }
