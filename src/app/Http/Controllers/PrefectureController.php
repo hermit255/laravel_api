f1a240f278639473252e11e7b1d02d75cb2653d2
Hiroshi <hermit255@gmail.com>
2019-08-16 19:09:24 +0900

    都道府県APIを利用したビューを追加

---
+++ src/app/Http/Controllers/PrefectureController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Prefecture;
+use App\Http\Controllers\AppController;
+
+class PrefecturesController extends AppController
+{
+    /**
+     *
+     * @return Illuminate\Support\Facades\View
+     */
+    public function list()
+    {
+        // cURLコマンドを使うのが正統派だが、コントローラーとして呼び出すこともできる
+        $wrapperApi = app()->make('App\Http\Controllers\Api\PrefecturesController');
+        $response = $wrapperApi->list(new \App\Prefecture());
+        $prefectures = json_decode($response->content());
+        return view('prefectures', ['prefectures' => $prefectures]);
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('prefectures/list', 'PrefecturesController@list');
