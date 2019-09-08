3a95e37259418788fc0f9bd434b099d801357d21
hermit255 <hermit255@gmail.com>
2019-09-08 21:03:15 +0900

    MODIFY: コントローラーの呼び出し方法を簡素化

--- src/app/Http/Controllers/PrefectureController.php
+++ src/app/Http/Controllers/PrefectureController.php
@@ -14,11 +14,7 @@ class PrefecturesController extends Controller
     public function list()
     {
         // cURLコマンドを使うのが正統派だが、コントローラーとして呼び出すこともできる
-        $Controller = app()->make('App\Http\Controllers\Api\PrefecturesController');
-        $useCase = app()->make('App\Contracts\GetPrefecturesIntaface');
-        $dbPrefectures = app()->make('App\Prefecture');
-
-        $response = $Controller->list($useCase, $dbPrefectures);
+        $response = app()->call('App\Http\Controllers\Api\PrefecturesController@list');
         $prefectures = json_decode($response->content());
         return view('prefectures', ['prefectures' => $prefectures]);
     }
