bfb46c654f3dfeb77226f9cabeac72c3cd9aa756
hermit255 <hermit255@gmail.com>
2019-09-08 16:14:53 +0900

    ADD: インデックスを差し替え

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('index');
 });
 Route::get('prefectures/list', 'PrefecturesController@list');
